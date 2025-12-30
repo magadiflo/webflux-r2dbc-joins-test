abb8a7b9927f18bf2d38682a2d83e18bea14144f
Martín Díaz <magadiflo@gmail.com>
2025-12-29 23:06:18 -0500

    Reviewing project

---
+++ src/main/java/dev/magadiflo/app/WebfluxCrudTestApplication.java
@@ -0,0 +1,13 @@
+package dev.magadiflo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCrudTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxCrudTestApplication.class, args);
+    }
+
+}
