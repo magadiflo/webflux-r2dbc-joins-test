b704ac15c91a84383c9bde023add8891b535441d
Martín Díaz <magadiflo@gmail.com>
2026-01-06 23:24:15 -0500

    Entidad Department

---
+++ src/main/java/dev/magadiflo/app/entity/Department.java
@@ -0,0 +1,52 @@
+package dev.magadiflo.app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+@Table(name = "departments")
+public class Department {
+    @Id
+    private Long id;
+    private String name;
+    private Employee manager;
+    @Builder.Default
+    private List<Employee> employees = new ArrayList<>();
+
+    public Optional<Employee> getManager() {
+        return Optional.ofNullable(this.manager);
+    }
+
+    public static Mono<Department> fromRows(List<Map<String, Object>> rows) {
+        Map<String, Object> rowsFirst = rows.getFirst();
+        Department department = Department.builder()
+                .id(((Number) rowsFirst.get("d_id")).longValue())
+                .name((String) rowsFirst.get("d_name"))
+                .manager(Employee.managerFromRow(rowsFirst))
+                .employees(rows.stream()
+                        .map(Employee::fromRow)
+                        .filter(Objects::nonNull)
+                        .toList())
+                .build();
+
+        return Mono.just(department);
+    }
+}
