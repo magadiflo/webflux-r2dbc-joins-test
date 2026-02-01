44c77ab37f2218683b377218d1018a9c8097b0bb
Martín Díaz <magadiflo@gmail.com>
2026-01-31 20:03:47 -0500

    Configuración inicial del Test EmployeeRepositoryTest

--- src/test/java/dev/magadiflo/app/integration/repository/EmployeeRepositoryTest.java
+++ src/test/java/dev/magadiflo/app/integration/repository/EmployeeRepositoryTest.java
@@ -1,11 +1,47 @@
 package dev.magadiflo.app.integration.repository;
 
 import dev.magadiflo.app.config.TestDatabaseConfig;
+import dev.magadiflo.app.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @DataR2dbcTest
 @ContextConfiguration(classes = TestDatabaseConfig.class)
 class EmployeeRepositoryTest {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private DatabaseClient databaseClient;
+
+    private static String DATA_SQL;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        Resource dataResource = new ClassPathResource("sql-test/data.sql");
+        Path dataPath = Paths.get(dataResource.getURI());
+        byte[] dataRead = Files.readAllBytes(dataPath);
+        DATA_SQL = new String(dataRead);
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.databaseClient
+                .sql(DATA_SQL)
+                .fetch()
+                .rowsUpdated()
+                .block();
+    }
 }
