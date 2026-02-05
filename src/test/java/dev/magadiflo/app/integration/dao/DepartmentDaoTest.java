d623f19603b2fe431a51a2d5ad3d00f5aa94679a
Martín Díaz <magadiflo@gmail.com>
2026-02-04 23:39:08 -0500

    DepartmentDaoTest - shouldFindAllDepartments

--- src/test/java/dev/magadiflo/app/integration/dao/DepartmentDaoTest.java
+++ src/test/java/dev/magadiflo/app/integration/dao/DepartmentDaoTest.java
@@ -1,13 +1,17 @@
 package dev.magadiflo.app.integration.dao;
 
 import dev.magadiflo.app.dao.DepartmentDao;
+import dev.magadiflo.app.entity.Department;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.r2dbc.core.DatabaseClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -40,5 +44,17 @@ class DepartmentDaoTest {
                 .fetch()
                 .rowsUpdated()
                 .block();
+    }
+
+    @Test
+    void shouldFindAllDepartments() {
+        Flux<Department> departmentFlux = this.departmentDao.findAll();
+
+        StepVerifier.create(departmentFlux)
+                .expectNextMatches(department -> department.getName().equals("Tecnología"))
+                .expectNextMatches(department -> department.getName().equals("Ventas"))
+                .expectNextMatches(department -> department.getName().equals("Legal"))
+                .expectNextMatches(department -> department.getName().equals("Soporte"))
+                .verifyComplete();
     }
 }
