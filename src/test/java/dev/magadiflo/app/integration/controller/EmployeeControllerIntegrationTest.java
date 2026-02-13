4cc77d6a043bb62858d6a8fcda9e255befb5b8c9
Martín Díaz <magadiflo@gmail.com>
2026-02-13 10:27:30 -0500

    EmployeeControllerIntegrationTest - shouldReturnAllEmployees_whenNoFiltersProvided

--- src/test/java/dev/magadiflo/app/integration/controller/EmployeeControllerIntegrationTest.java
+++ src/test/java/dev/magadiflo/app/integration/controller/EmployeeControllerIntegrationTest.java
@@ -1,11 +1,27 @@
 package dev.magadiflo.app.integration.controller;
 
 import dev.magadiflo.app.config.TestDatabaseConfig;
+import dev.magadiflo.app.dto.EmployeeResponse;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tests de integración para EmployeeController.
@@ -40,5 +56,49 @@ class EmployeeControllerIntegrationTest {
     @Autowired
     private WebTestClient webTestClient;
 
-    //TODO: implementar test
+    @Autowired
+    private DatabaseClient databaseClient;
+
+    private static String dataSql;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        Resource dataResource = new ClassPathResource("sql-test/data.sql");
+        Path dataPath = Paths.get(dataResource.getURI());
+        byte[] dataRead = Files.readAllBytes(dataPath);
+        dataSql = new String(dataRead);
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.databaseClient
+                .sql(dataSql)
+                .fetch()
+                .rowsUpdated()
+                .block();
+    }
+
+    // ===== GET /api/v1/employees/stream - findAllEmployees =====
+    @Test
+    void shouldReturnAllEmployees_whenNoFiltersProvided() {
+        // when
+        WebTestClient.ResponseSpec response = this.webTestClient.get()
+                .uri("/api/v1/employees/stream")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange();
+
+        // then
+        response.expectStatus().isOk()
+                .expectHeader().contentTypeCompatibleWith(MediaType.TEXT_EVENT_STREAM)
+                .expectBodyList(EmployeeResponse.class)
+                .hasSize(7) // 7 empleados en src/test/resources/sql-test/data.sql
+                .consumeWith(result -> {
+                    List<EmployeeResponse> employeeResponses = result.getResponseBody();
+                    assertThat(employeeResponses)
+                            .isNotNull();
+                    assertThat(employeeResponses.getFirst())
+                            .extracting(EmployeeResponse::id, EmployeeResponse::firstName)
+                            .containsExactly(1L, "Martín");
+                });
+    }
 }
