273920a397679aba9017967a4aa6bb9e226e4f3c
Martín Díaz <magadiflo@gmail.com>
2026-02-13 11:35:11 -0500

    EmployeeControllerIntegrationTest - shouldUpdateEmployee_whenValidDataProvided

--- src/test/java/dev/magadiflo/app/integration/controller/EmployeeControllerIntegrationTest.java
+++ src/test/java/dev/magadiflo/app/integration/controller/EmployeeControllerIntegrationTest.java
@@ -295,40 +295,45 @@ class EmployeeControllerIntegrationTest {
                 .jsonPath("$.errors.fullTime[0]").isEqualTo("must not be null");
     }
 
-//    // ===== PUT /api/v1/employees/{employeeId} - updateEmployee =====
-//    @Test
-//    void shouldUpdateEmployee_whenValidDataProvided() {
-//        // given
-//        Long employeeId = 1L;
-//        EmployeeRequest request = EmployeeFixture.createDefaultRequest();
-//        EmployeeResponse employeeResponse = new EmployeeResponse(
-//                employeeId,
-//                request.firstName(),
-//                request.lastName(),
-//                request.position(),
-//                request.fullTime()
-//        );
-//
-//        when(this.employeeService.updateEmployee(employeeId, request))
-//                .thenReturn(Mono.just(employeeResponse));
-//
-//        // when
-//        WebTestClient.ResponseSpec response = this.webTestClient.put()
-//                .uri("/api/v1/employees/{employeeId}", employeeId)
-//                .contentType(MediaType.APPLICATION_JSON)      // Tipo de contenido del request (lo que envío)
-//                .accept(MediaType.APPLICATION_JSON)  // Tipo de contenido esperado en la respuesta (Accept header)
-//                .bodyValue(request)
-//                .exchange();
-//
-//        // then
-//        response.expectStatus().isOk()
-//                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-//                .expectBody(EmployeeResponse.class)
-//                .isEqualTo(employeeResponse);
-//
-//        verify(this.employeeService).updateEmployee(employeeId, request);
-//    }
-//
+    // ===== PUT /api/v1/employees/{employeeId} - updateEmployee =====
+    @Test
+    void shouldUpdateEmployee_whenValidDataProvided() {
+        // given
+        Long employeeId = 1L;
+        EmployeeRequest request = EmployeeFixture.createDefaultRequest();
+
+        // when
+        WebTestClient.ResponseSpec response = this.webTestClient.put()
+                .uri("/api/v1/employees/{employeeId}", employeeId)
+                .contentType(MediaType.APPLICATION_JSON)      // Tipo de contenido del request (lo que envío)
+                .accept(MediaType.APPLICATION_JSON)  // Tipo de contenido esperado en la respuesta (Accept header)
+                .bodyValue(request)
+                .exchange();
+
+        // then
+        response.expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(employeeId)
+                .jsonPath("$.firstName").isEqualTo(request.firstName())
+                .jsonPath("$.lastName").isEqualTo(request.lastName())
+                .jsonPath("$.position").isEqualTo(request.position())
+                .jsonPath("$.fullTime").isEqualTo(request.fullTime());
+
+        // Verifica que el cambio persiste en la BD
+        this.webTestClient.get()
+                .uri("/api/v1/employees/{employeeId}", employeeId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(EmployeeResponse.class)
+                .consumeWith(result -> {
+                    EmployeeResponse employeeResponse = result.getResponseBody();
+                    assertThat(employeeResponse)
+                            .extracting(EmployeeResponse::firstName)
+                            .isEqualTo(request.firstName());
+                });
+    }
+
 //    @Test
 //    void shouldReturn404_whenUpdatingNonExistentEmployee() {
 //        // given
